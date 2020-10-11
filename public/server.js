e3fbcfd1fc0b29e4d490c4c552eae98a3d6f4d87
JacobDeCastroJohnson <Jacob.DeCastro.J@gmail.com>
2020-10-10 21:14:25 -0700

    Integrate all team member AWS IP routes into proxy handling

--- public/server.js
+++ public/server.js
@@ -5,25 +5,20 @@ const path = require('path');
 const cors = require('cors');
 const morgan = require('morgan');
 
-
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 app.use(cors());
 app.use(morgan('dev'));
-// app.use('/', express.static(path.join(__dirname, 'public')))
+
 app.use(express.static('public'));
-// app.use(express.static('client'))
 
-app.use('/listing', createProxyMiddleware({target:'http://54.183.243.19', changeOrigin: true}));
-app.use('/favorites', createProxyMiddleware({target:'http://54.183.243.19', changeOrigin: true}));
-// app.use('/api/booking/listing/:listingId', createProxyMiddleware({target:'http://localhost:3002', changeOrigin: true}));
+
+app.use('/listing', createProxyMiddleware({target:'http://54.67.5.9/', changeOrigin: true}));
+app.use('/favorites', createProxyMiddleware({target:'http://54.67.5.9/', changeOrigin: true}));
+app.use('/api/booking/listing/', createProxyMiddleware({target:'http://3.101.63.34', changeOrigin: true}));
 // app.use('/api/booking/listing/:reservations', createProxyMiddleware({target:'http://localhost:3002', changeOrigin: true}));
-// app.use('/api/reviews/:id', createProxyMiddleware({target:'http://localhost:3003', changeOrigin: true}));
+app.use('/api/reviews/:id', createProxyMiddleware({target:'http://54.67.90.25/', changeOrigin: true}));
 app.use('/api/more-places', createProxyMiddleware({target:'http://13.57.209.191/', changeOrigin: true}));
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World from Jacob proxy!')
-// })
 
 app.listen(port, () => {
   console.log(`Proxy listening at http://localhost:${port}`)
